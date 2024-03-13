ba557358fbf89869763b1997e740bcc96787b151
heyscottn <heyscottn@gmail.com>
2024-03-13 15:53:01 -0500

    mm

--- drone.py
+++ drone.py
@@ -3,7 +3,6 @@ import pygame
 # TODO
 # create the Drone class
 # a drone has 4 variables (must be received as parameters): x, y, xv, yv
-# each drone should have a radius of 10 pixels
-
 # and two methods: move, draw
+# note that 'draw' should draw a circle with radius 10 pixels
 
